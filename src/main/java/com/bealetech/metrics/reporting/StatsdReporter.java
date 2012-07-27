24cc13f583eb0c76ff75678e22080bff08c8b21d
Michael Rose <elementation@gmail.com>
2012-07-27 14:38:12 -0600

    Removing 'Graphite' and replacing with StatsD in errors -- very confusing

--- src/main/java/com/bealetech/metrics/reporting/StatsdReporter.java
+++ src/main/java/com/bealetech/metrics/reporting/StatsdReporter.java
@@ -63,7 +63,7 @@ public class StatsdReporter extends AbstractPollingReporter implements MetricPro
     }
 
     public StatsdReporter(MetricsRegistry metricsRegistry, String prefix, MetricPredicate predicate, UDPSocketProvider socketProvider, Clock clock, VirtualMachineMetrics vm) throws IOException {
-        this(metricsRegistry, prefix, predicate, socketProvider, clock, vm, "graphite-reporter");
+        this(metricsRegistry, prefix, predicate, socketProvider, clock, vm, "statsd-reporter");
     }
 
     public StatsdReporter(MetricsRegistry metricsRegistry, String prefix, MetricPredicate predicate, UDPSocketProvider socketProvider, Clock clock, VirtualMachineMetrics vm, String name) throws IOException {
@@ -113,9 +113,9 @@ public class StatsdReporter extends AbstractPollingReporter implements MetricPro
             socket.send(packet);
         } catch (Exception e) {
             if (LOG.isDebugEnabled()) {
-                LOG.debug("Error writing to Graphite", e);
+                LOG.debug("Error writing to StatsD", e);
             } else {
-                LOG.warn("Error writing to Graphite: {}", e.getMessage());
+                LOG.warn("Error writing to StatsD: {}", e.getMessage());
             }
             if (writer != null) {
                 try {
@@ -302,7 +302,7 @@ public class StatsdReporter extends AbstractPollingReporter implements MetricPro
             writer.write('\n');
             writer.flush();
         } catch (IOException e) {
-            LOG.error("Error sending to Graphite:", e);
+            LOG.error("Error sending to StatsD:", e);
         }
     }
 
